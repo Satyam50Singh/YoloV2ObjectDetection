9fa3dc78a0ca0cf72a4c7443ea1dac0ac387dd79
Satyam Singh <singh50satyam@gmail.com>
2022-09-10 11:40:04 +0530

    UI Changes Addedd

--- app/src/main/java/satya/app/objectdetectionyolov2/MainActivity.java
+++ app/src/main/java/satya/app/objectdetectionyolov2/MainActivity.java
@@ -2,8 +2,10 @@ package satya.app.objectdetectionyolov2;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,13 +16,17 @@ import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -42,9 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
     // UI Controls
     ImageView imageView;
-    TextView tvNoOfObjectDetected, tvPriority, tvClassName;
+    TextView tvNoOfObjectDetected, tvPriority, tvClassName, tvPValue, tvNmsValue;
     RelativeLayout rlProgressBar;
-    Button btnCaptureImage;
+    AppCompatButton btnCaptureImage;
+    SeekBar sbPThreshold, sbNmsThreshold;
+    private static final String TAG = "MainActivity";
+    float pThreshold = 0.5f, nmsThreshold = 0.5f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initUI();
+
+        setListeners();
     }
 
     // method to initialise controls
@@ -62,6 +73,50 @@ public class MainActivity extends AppCompatActivity {
         tvClassName = findViewById(R.id.tvClassName);
         rlProgressBar = findViewById(R.id.common_progress_bar);
         btnCaptureImage = findViewById(R.id.btn_capture_image);
+        sbPThreshold = findViewById(R.id.sb_p_threshold);
+        sbNmsThreshold = findViewById(R.id.sb_nms_threshold);
+        tvPValue = findViewById(R.id.tv_p_value);
+        tvNmsValue = findViewById(R.id.tv_nms_value);
+    }
+
+    private void setListeners() {
+        sbPThreshold.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int val, boolean b) {
+                pThreshold = (float) val / 100;
+                Log.e(TAG, "sbPThreshold: " + val + "  " + pThreshold);
+                tvPValue.setText(String.valueOf(pThreshold));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sbNmsThreshold.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int val, boolean b) {
+                nmsThreshold = (float) val / 100;
+                Log.e(TAG, "sbNmsThreshold: " + val + "  " + nmsThreshold);
+                tvNmsValue.setText(String.valueOf(nmsThreshold));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         btnCaptureImage.setOnClickListener(view -> {
             try {
@@ -76,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // check camera and gallery permissions.
-    public boolean checkAndRequestPermissions() {
+    private boolean checkAndRequestPermissions() {
         try {
             int storagePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
             int cameraPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
@@ -193,6 +248,8 @@ public class MainActivity extends AppCompatActivity {
 
     // method to classify Image Objects
     private void classifyImage(Bitmap image) {
+        rlProgressBar.setVisibility(View.VISIBLE);
+
         try {
             Yolov2 model = Yolov2.newInstance(this);
 
@@ -217,15 +274,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+            // p - threshold
             TensorBuffer inputFeature1 = TensorBuffer.createFixedSize(new int[]{1, 1}, DataType.FLOAT32);
-            ByteBuffer byteBuffer2 = ByteBuffer.allocateDirect(4);
-            inputFeature1.loadBuffer(byteBuffer2);
-            byteBuffer2.order(ByteOrder.nativeOrder());
-            byteBuffer2.putFloat((float) 0.5);
+            ByteBuffer byteBuffer1 = ByteBuffer.allocateDirect(4);
+            inputFeature1.loadBuffer(byteBuffer1);
+            byteBuffer1.order(ByteOrder.nativeOrder());
+            byteBuffer1.putFloat(pThreshold);
 
+            // nms - threshold
+            TensorBuffer inputFeature2 = TensorBuffer.createFixedSize(new int[]{1, 1}, DataType.FLOAT32);
+            ByteBuffer byteBuffer2 = ByteBuffer.allocateDirect(4);
+            inputFeature2.loadBuffer(byteBuffer2);
+            byteBuffer2.order(ByteOrder.nativeOrder());
+            byteBuffer2.putFloat(nmsThreshold);
 
             // Runs model inference and gets result.
-            Yolov2.Outputs outputs = model.process(inputFeature0, inputFeature1, inputFeature1);
+            Yolov2.Outputs outputs = model.process(inputFeature0, inputFeature1, inputFeature2);
 
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
             float[] noOfDetection = outputFeature0.getFloatArray();
@@ -236,47 +300,58 @@ public class MainActivity extends AppCompatActivity {
             float[] probability = outputFeature1.getFloatArray();
             tvPriority.setText(R.string.probability);
             for (int i = 0; i < totalObjects; i++) {
-                tvPriority.append(probability[i] * 100 + "%  , ");
+                tvPriority.append(probability[i] * 100 + "%, ");
             }
 
             TensorBuffer outputFeature2 = outputs.getOutputFeature2AsTensorBuffer();
             float[] coordinates = outputFeature2.getFloatArray();
 
-            // Code to draw bounding boxes  ----------------------------------------------------------------------------------------------
-            Canvas canvas = new Canvas(image);
-            Paint paint = new Paint();
-            paint.setColor(Color.BLACK);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(10);
-
-            for (int k = 0; k < totalObjects * 4; k += 4) {
-                float leftx = (float) Math.ceil(coordinates[k]);
-                float topy = (float) Math.ceil(coordinates[k + 1]);
-                float rightx = (float) Math.ceil(coordinates[k + 2]) + (float) Math.ceil(coordinates[k]);
-                float bottomy = (float) Math.ceil(coordinates[k + 1]) + (float) Math.ceil(coordinates[k + 3]);
-                canvas.drawRect(leftx, topy, rightx, bottomy, paint);
-            }
-            imageView.setImageBitmap(image);
-
-            //-------------------------------------------------------------------------------------------------------------------------------------
-
             TensorBuffer outputFeature3 = outputs.getOutputFeature3AsTensorBuffer();
-            float[] index = outputFeature3.getFloatArray();
-            int i = (int) index[0];
+            float[] classIndexes = outputFeature3.getFloatArray();
+            int i = (int) classIndexes[0];
 
             String[] class_names = new String[]{"aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
                     "dining table", "dog", "horse", "motorbike", "person", "potted plant", "sheep", "sofa", "train", "tv monitor"};
 
             tvClassName.setText(R.string.class_name);
             for (int j = 0; j < totalObjects; j++) {
-                tvClassName.append(class_names[(int) index[j]].toUpperCase() + "  , ");
+                tvClassName.append(class_names[(int) classIndexes[j]].toUpperCase() + ", ");
             }
-            tvClassName.setTextColor(getColor(R.color.purple_500));
+
+            // Code to draw bounding boxes  ----------------------------------------------------------------------------------------------
+            Canvas canvas = new Canvas(image);
+            Paint paint = new Paint();
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(6);
+
+            Paint textStyle = new Paint(Paint.ANTI_ALIAS_FLAG);
+            textStyle.setColor(Color.BLACK);
+            Resources resources = getResources();
+            float scale = resources.getDisplayMetrics().density;
+            textStyle.setTextSize((int) (15 * scale));
+            textStyle.setColor(getColor(R.color.black));
+
+            int indexVal = 0;
+            for (int k = 0; k < totalObjects * 4; k += 4) {
+                float leftx = (float) Math.ceil(coordinates[k]);
+                float topy = (float) Math.ceil(coordinates[k + 1]);
+                float rightx = (float) Math.ceil(coordinates[k + 2]) + (float) Math.ceil(coordinates[k]);
+                float bottomy = (float) Math.ceil(coordinates[k + 1]) + (float) Math.ceil(coordinates[k + 3]);
+                canvas.drawRect(leftx, topy, rightx, bottomy, paint);
+                canvas.drawText(class_names[(int) classIndexes[indexVal++]].toUpperCase(), leftx, topy, textStyle);
+            }
+            imageView.setImageBitmap(image);
+
+            //-------------------------------------------------------------------------------------------------------------------------------------
+
 
             // Releases model resources if no longer used.
             model.close();
+            rlProgressBar.setVisibility(View.GONE);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
